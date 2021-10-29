979d1e36e770da66e699a63323dac4b696271fdf
Rick0317 <201507037f@edu.nishiyamato.ed.jp>
2021-10-28 21:26:28 -0400

    active

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,7 +4,7 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
-  
+
   belongs_to :user
   has_many   :comments, dependent: :destroy
   has_one_attached :image
